4e5fd9754a4f6585d903c837381ddc2f58eb7c5a
Thisseas X <thisseasx@yahoo.com>
2020-04-10 21:14:08 +0300

    Updates

---
+++ precache-manifest.c1007cc89ce3b625152d82027f155f1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9de63716ef4ae8b2f51404fa1197b87",
+    "url": "/react-intro-3/index.html"
+  },
+  {
+    "revision": "b5abdd1795ab3bbe2565",
+    "url": "/react-intro-3/static/css/main.97d26ce4.chunk.css"
+  },
+  {
+    "revision": "76e0ac1cbd168993eac8",
+    "url": "/react-intro-3/static/js/2.f3809c9b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-intro-3/static/js/2.f3809c9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5abdd1795ab3bbe2565",
+    "url": "/react-intro-3/static/js/main.9bb4ab6c.chunk.js"
+  },
+  {
+    "revision": "857d8c7d2e934b977ad6",
+    "url": "/react-intro-3/static/js/runtime-main.892ae394.js"
+  }
+]);
